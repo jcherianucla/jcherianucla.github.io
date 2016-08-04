6226e79d5f1b387cd6a38bbea31156de597bcf0a
jcherianucla <jcherian@ucla.edu>
2016-08-03 23:04:55 -0700

    Added some stuff to work page

--- new/javascripts/main.js
+++ new/javascripts/main.js
@@ -2,15 +2,19 @@ $(document).ready(function(){
 	$("#w-x").hide();
 	$("#n-x").hide();
 	$("#c-x").hide();
+	$(".work-container").hide();
 	$(".work").click(function(){
-		$("html").css('background', 'url(images/bg_work.png) no-repeat center center fixed');
+		//$("html").css('background', 'url(images/bg_work.png) no-repeat center center fixed');
 		$(".abt-container").fadeOut();
+		$(".work-container").fadeIn();
 		$("#a-x").hide();
 		$("#w-x").fadeIn();
 	})
 	$(".about").click(function(){
-		$("html").css('background', 'url(images/bg_about.png) no-repeat center center fixed');
+		//$("html").css('background', 'url(images/bg_about.png) no-repeat center center fixed');
+		$("#w-x").hide();
 		$("#a-x").fadeIn();
+		$(".work-container").fadeOut();
 		$(".abt-container").fadeIn();
 	})
 
