ac0d9e71ac49168fb6a5664b1c792b941e81afc6
jcherianucla <jcherian@ucla.edu>
2016-08-07 23:24:26 -0700

    Fixed one way transition effect

--- new/javascripts/main.js
+++ new/javascripts/main.js
@@ -20,8 +20,29 @@ $(document).ready(function(){
 	$("#code").click(function(){
 		$(".software").removeClass("col-xs-6");
 		$(".design").removeClass("col-xs-6");
-		$(".software").css("width", "80%");
-		$(".desgin").css("width", "20%");
+		$(".software").css("width", "90%");
+		$("#code").hide();
+		$(".design").css("width", "10%");
+	});
+	// Instantiate the Bootstrap carousel
+	$('.multi-item-carousel').carousel({
+		interval: false
+	});
+
+	// for every slide in carousel, copy the next slide's item in the slide.
+	// Do the same for the next, next item.
+	$('.multi-item-carousel .item').each(function(){
+		var next = $(this).next();
+		if (!next.length) {
+			next = $(this).siblings(':first');
+		}
+		next.children(':first-child').clone().appendTo($(this));
+
+		if (next.next().length>0) {
+			next.next().children(':first-child').clone().appendTo($(this));
+		} else {
+			$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+		}
 	});
 
 });
