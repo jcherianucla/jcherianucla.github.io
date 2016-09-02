d9dbeb9631bef0a8029b9e25c9fc2e66d0a877ca
jcherianucla <jcherian@ucla.edu>
2016-09-02 15:41:27 -0700

    Fixed up some bugs, and refactored JS

--- new/javascript/main.js
+++ new/javascript/main.js
@@ -1,5 +1,55 @@
+function determineContent(elem, should_hide){
+	if(should_hide){
+		$(elem).hide();
+	} else {
+		$(elem).fadeIn(500);
+	}
+};
+function hideAllContent(){
+	var content_list = $('.main-content').children();
+	for(var i = 0; i < content_list.length; i++){
+		determineContent(content_list[i], true);
+	}
+};
+function select(elem, sel){
+	if(sel){
+		$(elem).children('.off').hide();
+		$(elem).children('.on').show();
+	} else {
+		$(elem).children('.on').hide();
+		$(elem).children('.off').show();
+	}
+};
+function changeContent(event){
+	var nav_elem_list = $('.items').children();
+	for(var i = 0; i < nav_elem_list.length; i++){
+		var temp_elem = nav_elem_list[i];
+		select(temp_elem, false);
+	}
+	var target = event.target;
+	console.log(target);
+	select(target, true);
+	hideAllContent();
+	var content_list = $('.main-content').children();
+	for(var i = 0; i < content_list.length; i++){
+		if(target.hash === ("#"+content_list[i].id)){
+			determineContent(content_list[i], false);
+		}
+		if(target.hash === "#work"){
+			$('.work-contains').fadeIn();
+		} else {
+			$('.work-contains').hide();
+		}
+	}
+
+};
 $(document).ready(function () {
 	$('.on').hide();
-	$('.items').children().eq(0).children('.off').hide();
-	$('.items').children().eq(0).children('.on').show();
+	hideAllContent();
+	$('.about').show();
+	$('.work-contains').hide();
+	var active = $('.items').children().first();
+	select(active, true);
+
 });
+
